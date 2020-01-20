7963fc4bc540226a07bf91fcda7f30e6a53f4729
Paško Berišić <paberisi@vsite.hr>
2020-01-20 10:18:42 +0100

    Half resolve pictureBox / drawing

--- CshProject/Form1.Designer.cs
+++ CshProject/Form1.Designer.cs
@@ -30,14 +30,14 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBoxArea = new System.Windows.Forms.TextBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 333);
+            this.button1.Location = new System.Drawing.Point(12, 339);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(122, 35);
             this.button1.TabIndex = 0;
@@ -48,20 +48,20 @@
             // label1
             // 
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label1.Location = new System.Drawing.Point(549, 348);
+            this.label1.Location = new System.Drawing.Point(542, 354);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(148, 20);
             this.label1.TabIndex = 1;
             this.label1.Text = "Ukupna površina:";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            
             // 
-            // textBox1
+            // textBoxArea
             // 
-            this.textBox1.Location = new System.Drawing.Point(696, 348);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(135, 20);
-            this.textBox1.TabIndex = 2;
+            this.textBoxArea.Location = new System.Drawing.Point(696, 354);
+            this.textBoxArea.Name = "textBoxArea";
+            this.textBoxArea.Size = new System.Drawing.Size(135, 20);
+            this.textBoxArea.TabIndex = 2;
             // 
             // pictureBox1
             // 
@@ -70,14 +70,16 @@
             this.pictureBox1.Size = new System.Drawing.Size(814, 315);
             this.pictureBox1.TabIndex = 3;
             this.pictureBox1.TabStop = false;
+            
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
             this.ClientSize = new System.Drawing.Size(843, 383);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBoxArea);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
@@ -92,7 +94,7 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBoxArea;
         private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
--- CshProject/Form1.cs
+++ CshProject/Form1.cs
@@ -12,19 +12,54 @@ namespace CshProject
 {
     public partial class Form1 : Form
     {
+        Rectangle rectangle;
+
+        Point StartPoint;
+        Point EndPoint;
+
+        int xHeight, yWeight, sum = 0;
+
+        bool IsMouseDown = false;
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void label1_Click(object sender, EventArgs e)
+       
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
         {
+            IsMouseDown = true;
 
+            StartPoint = e.Location;
         }
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (IsMouseDown == true)
+            {
+                EndPoint = e.Location;
 
+                Refresh();
+            }
+        }
         private void button1_Click(object sender, EventArgs e)
         {
+            if (pictureBox1.Image != null)
+            {
+                pictureBox1.Image = null;
+                
+                textBoxArea.Text = "0";
 
+                sum = 0;
+
+                Invalidate();
+            }
+        }
+
+        int Area(int xHeight, int yWeight)
+        {
+            return xHeight * yWeight;
         }
     }
 }
